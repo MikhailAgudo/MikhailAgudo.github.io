ad227f1d1095994a44d81f4545032ab1505c63fa
MikhailAgudo <mikhail.agudo@gmail.com>
2021-01-20 14:56:40 +0800

    Adjusted for phones.

---
+++ src/Portfolio/Project.js
@@ -0,0 +1,16 @@
+const Project = (name, description, imageLink, liveLink) => {
+    // $name and $description are strings, to be displayed
+    // by ProjectDisplay.js
+    // $imageLink has the same function.
+    // $live is used to link for a live version of the
+    // project. If the value is null, then ProjectDisplay.js
+    // does not produce a button for it.
+
+    return {
+        name,
+        description,
+        imageLink,
+        live: liveLink
+    }
+}
+
