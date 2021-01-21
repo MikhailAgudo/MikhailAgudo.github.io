995c21016832ae55a50388cab1cd240760a0c731
MikhailAgudo <mikhail.agudo@gmail.com>
2021-01-21 14:24:12 +0800

    Changed the portfolio code links to their proper repositories.

--- src/Resume.js
+++ src/Resume.js
@@ -15,7 +15,7 @@ const Resume = (() => {
             "Calculator",
             "This is a calculator that takes input from a string and solves it using MDAS.",
             "images/calculator.png",
-            "https://github.com/MikhailAgudo/JS-Full-Stack-Practice/tree/master/calculator",
+            "https://github.com/MikhailAgudo/MikhailAgudo.github.io/tree/master/docs/calculator",
             "calculator/calculator.html"
         ));
 
@@ -23,7 +23,7 @@ const Resume = (() => {
             "Etch-A-Sketch",
             "A simple app that lets users draw in different hues from white to black, like a a real Etch a Sketch.",
             "images/sketch.png",
-            "https://github.com/MikhailAgudo/JS-Full-Stack-Practice/tree/master/etch-a-sketch",
+            "https://github.com/MikhailAgudo/MikhailAgudo.github.io/tree/master/docs/etch-a-sketch",
             "etch-a-sketch/sketch.html"
         ));
 
@@ -31,7 +31,7 @@ const Resume = (() => {
             "Tic-Tac-Toe",
             "Tic-Tac-Toe game where players click on the buttons to decide their move. Does not include AI.",
             "images/tictactoe.png",
-            "",
+            "https://github.com/MikhailAgudo/MikhailAgudo.github.io/tree/master/docs/tic-tac-toe",
             "tic-tac-toe/tictactoe.html"
         ))
     }
