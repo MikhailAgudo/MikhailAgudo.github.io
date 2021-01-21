1726dfcc0585939eaaec622345cc65ea3b0bfa69
MikhailAgudo <mikhail.agudo@gmail.com>
2021-01-21 11:46:08 +0800

    Added functioning portfolio display.

--- src/Portfolio/ProjectDisplay.js
+++ src/Portfolio/ProjectDisplay.js
@@ -1,43 +1,71 @@
 const ProjectDisplay = (() => {
-    const display = (project) => {
+    const portfolioSection = document.querySelector(".portfolio-section");
+
+    const render = (project) => {
         let projectContainer = createContainer();
         let projectImage = createImage(project.imageLink);
-        let detailsContainer = createDetailsContainer();
+        let detailsContainer = createDetailsContainer(project.name, project.description, project.codeLink, project.liveLink);
+
+        appendProjectDetails(projectContainer, projectImage, detailsContainer);
+
+        appendProject(projectContainer);
+    }
+
+    const appendProject = (projectContainer) => {
+        portfolioSection.appendChild(projectContainer);
+    }
+
+    const appendProjectDetails = (projectContainer, projectImage, detailsContainer) => {
+        projectContainer.appendChild(projectImage);
+        projectContainer.appendChild(detailsContainer);
     }
 
     const createContainer = () => {
         let projectContainer = document.createElement("div");
 
         projectContainer.classList.add("project-container");
+        
+        return projectContainer;
     }
 
     const createDetailsContainer = (name, description, codeLink, liveLink) => {
         let detailsContainer = document.createElement("div");
         let projectName = createName(name);
         let projectDescription = createDescription(description);
-        let projectCode = createLink(codeLink);
-
-        if (liveLink !== null) {
-            let projectLive = createLink(liveLink);
-        }
+        let projectCode = createLink(codeLink, "View Code");
 
         detailsContainer.classList.add("project-details-container");
+
+        detailsContainer.appendChild(projectName);
+        detailsContainer.appendChild(projectDescription);
+        detailsContainer.appendChild(projectCode);
+
+        if (liveLink !== null) {
+            let projectLive = createLink(liveLink, "See it Live");
+            detailsContainer.appendChild(projectLive);
+        }
+
+        return detailsContainer;
     }
 
     const createName = (name) => {
         let projectName = document.createElement("h2");
+        projectName.textContent = name;
 
         return projectName;
     }
 
     const createDescription = (description) => {
         let projectDescription = document.createElement("p");
+        projectDescription.textContent = description;
 
         return projectDescription;
     }
 
-    const createLink = (codeLink) => {
-        let projectCode = document.createElement("codeLink");
+    const createLink = (codeLink, text) => {
+        let projectCode = document.createElement("a");
+        projectCode.href = codeLink;
+        projectCode.textContent = text;
 
         return projectCode;
     }
@@ -46,10 +74,12 @@ const ProjectDisplay = (() => {
         let projectImage = document.createElement("img");
 
         projectImage.src = imageLink;
+
+        return projectImage;
     }
 
     return {
-        display
+        render
     }
 })();
 
--- src/Resume.js
+++ src/Resume.js
@@ -6,19 +6,31 @@ const Resume = (() => {
     let projects = [];
     const render = () => {
         initiateProjects();
+        renderProjects();
         NavBar.render();
     }
 
     const initiateProjects = () => {
         let calculator = Project(
             "Calculator",
-            "",
-            "",
-            "",
-            ""
+            "This is a small project, to make a fairly capable calculator based on the Odin Project. The code does everything in the requirements and all the extra credits; save one. The project did not specify MDAS as a requirement, but this calculator will make use of it.",
+            "images/calculator.png",
+            "https://github.com/MikhailAgudo/JS-Full-Stack-Practice/tree/master/calculator",
+            "https://github.com/MikhailAgudo/JS-Full-Stack-Practice/tree/master/calculator"
         )
         
-        projects.shift(calculator);
+        projects.push(calculator);
+    }
+
+    const renderProjects = () => {
+        let newProject = projects.shift();
+        ProjectDisplay.render(newProject);
+
+        if (projects.length === 0) {
+            
+        } else {
+            renderProjects();
+        }
     }
 
     return {
